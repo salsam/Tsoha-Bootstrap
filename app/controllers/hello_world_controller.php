7ab2475844edede68a5a979a04353284e3ad23e1
Sami <sami.salonen705@gmail.com>
2016-12-21 15:31:11 +0200

    Aloitettu sivujen luonti

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,13 +4,18 @@ class HelloWorldController extends BaseController {
 
     public static function index() {
         // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-//   	  View::make('home.html');
-        echo 'Tämä on etusivu';
+        View::make('home.html');
+//        echo 'Tämä on etusivu';
     }
 
     public static function sandbox() {
         // Testaa koodiasi täällä
-        echo 'Hello World!';
+//        echo 'Hello World!';
+        View::make('helloworld.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
     }
 
 }
